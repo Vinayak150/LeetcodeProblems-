0c47a475a034c203963b5aa9fa2be2fc0cfdfdab
Vinayak Mahindrakar <103259839+Vinayak150@users.noreply.github.com>
2026-01-03 23:46:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (70%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -1,58 +1,19 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
-    vector<vector<int>> rows;
-    vector<vector<int>> dp;
-    int n;
+    int numOfWays(int n) {
+        const int MOD = 1e9 + 7;
 
-    void generateRows(){
-        for( int a =0;a <3;a++){
-            for(int b=0; b<3;b++){
-                for(int c =0;c<3;c++){
-                    if(a != b && b != c){
-                        rows.push_back({a,b,c});
+        long long  a =6;
+        long long b = 6;
 
-                    }
-                }
-            }
-        }
-    }
+        for( int i =2; i <= n; i++){
+            long long newA = ( a* 2 +b * 2) % MOD;
+            long long newB = ( a * 2  + b * 3) % MOD;
 
-    bool compatible(vector<int>& r1, vector<int>& r2){
-        for( int i =0;i < 3; i++){
-            if ( r1[i]==r2[i]) return false;
-        }
-        return true;
-    }
-
-    int dfs(int row, int prev){
-        if( row == n) return 1;
-
-        if( dp[row][prev]!= -1)
-        return dp[row][prev];
-
-        long long ways = 0;
-        for(int i =0; i< rows.size(); i++){
-            if (compatible(rows[prev], rows[i])) {
-                ways = (ways + dfs(row + 1, i)) % MOD;
-            }
+            a = newA;
+            b = newB;
         }
 
-        return dp[row][prev] = ways;
-    }
-
-    int numOfWays(int n_) {
-        n = n_;
-        generateRows();
-        dp.assign(n, vector<int>(rows.size(), -1));
-
-        long long ans = 0;
-
-        for( int i=0; i < rows.size(); i++){
-            ans = ( ans + dfs(1,i)) % MOD;
-        }
-
-        return ans;
-        
+        return ( a+b) % MOD;
     }
 };
