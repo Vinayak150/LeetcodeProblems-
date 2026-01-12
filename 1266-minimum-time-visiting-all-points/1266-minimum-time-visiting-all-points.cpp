2f9c5bb67f06a582278e529358b708553adda6ee
Vinayak Mahindrakar <103259839+Vinayak150@users.noreply.github.com>
2026-01-13 01:07:33 +0530

    Time: 0 ms (100%), Space: 13.9 MB (26.71%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -1,52 +1,24 @@
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int time = 0;
-        for (int i = 0; i < points.size() - 1; i++) {
+        int time =0;
+
+        for(int i = 0; i < points.size()-1; i++){
+
             int x1 = points[i][0];
             int y1 = points[i][1];
 
-            int x2 = points[i + 1][0];
-            int y2 = points[i + 1][1];
+            int x2 = points[i+1][0];
+            int y2 = points[i+1][1];
 
-            while (x1 != x2 || y1 != y2) {
+            int dx = abs(x2 -x1);
+            int dy = abs(y2-y1);
 
-                if (x1 < x2 && y1 < y2) {
-                    x1++;
-                    y1++;
-                }
-
-                else if (x1 < x2 && y1 > y2) {
-                    x1++;
-                    y1--;
-                } else if (x1 > x2 && y1 < y2) {
-                    x1--;
-                    y1++;
-                }
-
-                else if (x1 > x2 && y1 > y2) {
-                    x1--;
-                    y1--;
-                }
-
-                else if (x1 < x2) {
-                    x1++;
-                }
-
-                else if (x1 > x2) {
-                    x1--;
-                }
-
-                else if (y1 < y2) {
-                    y1++;
-                } else if (y1 > y2) {
-                    y1--;
-                }
-
-                time++;
-            }
+            time += max(dx,dy);
         }
 
         return time;
+
+        
     }
 };
