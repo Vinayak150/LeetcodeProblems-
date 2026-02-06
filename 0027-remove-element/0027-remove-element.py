106d3f24ccedaa3f1fc7eceb7f9a115467becefc
Vinayak Mahindrakar <103259839+Vinayak150@users.noreply.github.com>
2026-02-07 00:18:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (54.77%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -8,7 +8,7 @@ class Solution(object):
         k =0
         for i in range (len(nums)):
             if nums[i] != val:
-                nums[k] = nums[i]
+                nums[k]=nums[i]
                 k=k+1
 
         return k
