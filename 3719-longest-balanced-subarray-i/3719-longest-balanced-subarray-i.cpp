0de8284ee3853e4d25e53d8c543032601322cc0d
Vinayak Mahindrakar <103259839+Vinayak150@users.noreply.github.com>
2026-02-11 19:34:32 +0530

    Time: 1857 ms (75.37%), Space: 505.1 MB (57.97%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,25 +1,63 @@
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
-        
+
         int n = nums.size();
         int ans = 0;
-        for(int l =0;l<n;l++){
-            unordered_set<int> even;
-            unordered_set<int> odd;
-            for(int r =l;r<n;r++){
-                if(nums[r]%2 ==0){
-                    even.insert(nums[r]);
-                    
+
+        // Precompute total distinct evens and odds remaining from each index
+        vector<int> suffixEven(n+1, 0), suffixOdd(n+1, 0);
+        unordered_set<int> seenEven, seenOdd;
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffixEven[i] = suffixEven[i+1];
+            suffixOdd[i] = suffixOdd[i+1];
+
+            if (nums[i] % 2 == 0) {
+                if (!seenEven.count(nums[i])) {
+                    seenEven.insert(nums[i]);
+                    suffixEven[i]++;
                 }
-                else{
-                    odd.insert(nums[r]);
-                }
-                if(even.size()== odd.size()){
-                    ans = max(ans,r-l+1);
+            } else {
+                if (!seenOdd.count(nums[i])) {
+                    seenOdd.insert(nums[i]);
+                    suffixOdd[i]++;
                 }
             }
         }
+
+        for (int l = 0; l < n; l++) {
+
+            unordered_map<int,int> freq;
+            int distinctEven = 0;
+            int distinctOdd = 0;
+
+            for (int r = l; r < n; r++) {
+
+                freq[nums[r]]++;
+
+                if (freq[nums[r]] == 1) {
+                    if (nums[r] % 2 == 0)
+                        distinctEven++;
+                    else
+                        distinctOdd++;
+                }
+
+                if (distinctEven == distinctOdd)
+                    ans = max(ans, r - l + 1);
+
+                // 🔥 Early stopping condition
+                int remainingEven = suffixEven[r+1];
+                int remainingOdd = suffixOdd[r+1];
+
+                if (distinctEven > distinctOdd + remainingOdd)
+                    break;
+
+                if (distinctOdd > distinctEven + remainingEven)
+                    break;
+            }
+        }
+
         return ans;
     }
 };
