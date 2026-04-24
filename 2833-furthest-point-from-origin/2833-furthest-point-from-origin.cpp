eb21d09a7d691d689af4843d47a466cbabdc42a4
Vinayak Mahindrakar <103259839+Vinayak150@users.noreply.github.com>
2026-04-24 14:16:28 +0530

    Time: 1 ms (12.55%), Space: 9.3 MB (6.23%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int L=0,R=0,blank=0;
+        for(char c :moves){
+            if(c=='L')L++;
+            else if (c=='R')R++;
+            else blank++;
+
+        }
+        if(L>=R){
+            return L-R+blank;
+        }
+        else{
+            return R-L+blank;
+        }
+        
+    }
+};
