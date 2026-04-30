e8e5f96e4abe361b712881d18ccd84f3c207cbfb
Vinayak Mahindrakar <103259839+Vinayak150@users.noreply.github.com>
2026-05-01 03:01:37 +0530

    Time: 729 ms (84.12%), Space: 499.4 MB (63.53%) - LeetHub

---
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
+
+        // dp[i][j][c] = max score reaching (i,j) with cost c
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k+1, -1)));
+
+        
+        int startCost = (grid[0][0] == 0 ? 0 : 1);
+        if (startCost > k) return -1;
+
+        dp[0][0][startCost] = grid[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                for (int c = 0; c <= k; c++) {
+
+                    if (dp[i][j][c] == -1) continue;
+
+                    // move RIGHT
+                    if (j + 1 < n) {
+                        int val = grid[i][j+1];
+                        int cost = (val == 0 ? 0 : 1);
+
+                        if (c + cost <= k) {
+                            dp[i][j+1][c + cost] = max(
+                                dp[i][j+1][c + cost],
+                                dp[i][j][c] + val
+                            );
+                        }
+                    }
+
+                    // move DOWN
+                    if (i + 1 < m) {
+                        int val = grid[i+1][j];
+                        int cost = (val == 0 ? 0 : 1);
+
+                        if (c + cost <= k) {
+                            dp[i+1][j][c + cost] = max(
+                                dp[i+1][j][c + cost],
+                                dp[i][j][c] + val
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        
+        int ans = -1;
+        for (int c = 0; c <= k; c++) {
+            ans = max(ans, dp[m-1][n-1][c]);
+        }
+
+        return ans;
+    }
+};
