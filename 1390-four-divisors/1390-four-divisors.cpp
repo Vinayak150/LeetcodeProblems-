1fba16d21f09beb493749f359383b10880fe14ac
Vinayak Mahindrakar <103259839+Vinayak150@users.noreply.github.com>
2026-05-02 13:56:14 +0530

    Time: 6 ms (85.93%), Space: 16.3 MB (48.6%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -3,22 +3,38 @@ public:
     int sumFourDivisors(vector<int>& nums) {
         int totalSum = 0;
 
-        for(int num : nums){
-            int count = 0;
-            int sum = 0;
+        
+        for (int num : nums) {
+            int divisorCount = 0;   
+            int divisorSum = 0;     
+           
+            for (int d = 1; d * d <= num; d++) {
+                if (num % d == 0) {
+                    int other = num / d;
 
-            for(int i =1;i <= num;i++){
-                if( num % i == 0){
-                    count++;
-                    sum += i;
+                   
+                    divisorCount++;
+                    divisorSum += d;
+
+                    
+                    if (other != d) {
+                        divisorCount++;
+                        divisorSum += other;
+                    }
+
+                    
+                    if (divisorCount > 4) {
+                        break;
+                    }
                 }
-                if(count > 4) break;
             }
 
-            if(count == 4){
-                totalSum += sum;
+           
+            if (divisorCount == 4) {
+                totalSum += divisorSum;
             }
         }
+
         return totalSum;
     }
 };
