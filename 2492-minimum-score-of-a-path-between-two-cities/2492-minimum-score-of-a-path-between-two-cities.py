9b177bcda4b459ef0f9bcde9e3e9a1973315755a
Vinayak Mahindrakar <103259839+Vinayak150@users.noreply.github.com>
2026-07-05 02:19:49 +0530

    Time: 143 ms (79.96%), Space: 102.3 MB (22.04%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+
+        graph = defaultdict(list)
+
+        # Build graph
+        for u, v, w in roads:
+            graph[u].append((v, w))
+            graph[v].append((u, w))
+
+        visited = set()
+
+        ans = float('inf')
+
+        def dfs(node):
+            nonlocal ans
+
+            visited.add(node)
+
+            for nei, wt in graph[node]:
+
+                # Every edge in this component is a candidate
+                ans = min(ans, wt)
+
+                if nei not in visited:
+                    dfs(nei)
+
+        dfs(1)
+
+        return ans
